a43c7c2942e3b3239aaad94d891d6281a88a001c
Amiya Dey <amiya1999dey@gmail.com>
2025-01-15 11:37:37 +0530

    recipe-details page

--- recipe-recommender-frontend/src/app/components/recipe-details/recipe-details.component.ts
+++ recipe-recommender-frontend/src/app/components/recipe-details/recipe-details.component.ts
@@ -1,12 +1,47 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, RouterLink } from '@angular/router';
+import { RecipeService } from '../../services/recipe.service';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-recipe-details',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './recipe-details.component.html',
-  styleUrl: './recipe-details.component.css'
+  styleUrl: './recipe-details.component.css',
 })
 export class RecipeDetailsComponent {
+  recipe: any;
+  ingredients: string[] = [];
+  steps: string[] = [];
 
+  constructor(
+    private route: ActivatedRoute,
+    private recipeService: RecipeService
+  ) {}
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+    if (id) {
+      this.recipeService.getRecipeById(+id).subscribe({
+        next: (data) => {
+          this.recipe = {
+            ...data,
+            nutrition: data.nutrition || {},
+            related_recipes: data.related_recipes || [],
+          };
+          this.ingredients = this.recipe.ingredients.split(', ');
+          this.steps = this.recipe.steps.split(', ');
+        },
+        error: (err) => {
+          console.error('Error fetching recipe:', err);
+        },
+      });
+    }
+  }
+
+  addToFavorites(recipeId: number): void {
+    console.log(`Adding recipe ${recipeId} to favorites`);
+    // Implement favorite addition logic here
+  }
 }
